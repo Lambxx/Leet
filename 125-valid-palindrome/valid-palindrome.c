88517290935b916a97c3adca89a030b8eb3aff64
Luna Lamb <jasminelamb@proton.me>
2024-06-19 18:24:31 +0200

    Time: 5 ms (39.93%) | Memory: 6.1 MB (77.71%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.c
+++ 125-valid-palindrome/valid-palindrome.c
@@ -1,20 +1,23 @@
 bool isPalindrome(char* s) {
    
 
-    char* sEnd = s + strlen(s) -1;
-    char* fVal = s;
+    char* sEnd = s + strlen(s) -1; // Set end pointer to final char
+    char* fVal = s; // fVal keeps track of first pointer val
     
-    while(*s && *sEnd && (s<sEnd) ){
+    while(*s && *sEnd && (s<sEnd) ){ // While both pointers point at a character and s pointer is less than end 
         while (*s && !isalnum(*s) && (s<sEnd)){
-            s++;
+            s++; // Increment start to until its an AlphaNumeric Value
         } 
          while ( *sEnd && (s<sEnd) && !isalnum(*sEnd) && (sEnd > fVal)){
             sEnd--;
+            // Decrement start to until its an AlphaNumeric Value
         } 
        
        if(s<sEnd){
+        // Check s pointer is less than end 
         if(toupper(*s) != toupper(*sEnd)){
             return false;
+            // If dont match return false 
         }
         s++;
        sEnd--;
@@ -23,5 +26,6 @@ bool isPalindrome(char* s) {
         
     }
     return true;
+    // Is a palindrome 
 
 }
