c3c823aa346573b783bffe0d618b368c510e62e4
Luna Lamb <jasminelamb@proton.me>
2024-06-19 18:19:31 +0200

    Time: 6 ms (17.23%) | Memory: 6.4 MB (35.02%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.c
@@ -0,0 +1,27 @@
+bool isPalindrome(char* s) {
+   
+
+    char* sEnd = s + strlen(s) -1;
+    char* fVal = s;
+    
+    while(*s && *sEnd && (s<sEnd) && (sEnd > fVal)){
+        while (*s && !isalnum(*s) && (s<sEnd)){
+            s++;
+        } 
+         while ( *sEnd && (s<sEnd) && !isalnum(*sEnd) && (sEnd > fVal)){
+            sEnd--;
+        } 
+       
+       if(s<sEnd){
+        if(toupper(*s) != toupper(*sEnd)){
+            return false;
+        }
+        s++;
+       sEnd--;
+        }
+       
+        
+    }
+    return true;
+
+}
